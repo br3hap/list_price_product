5e805fbd36d2d555d4b8fed145abd7ed6e3acbc8
Breithner <Breithnerap@gmail.com>
2025-07-11 13:32:55 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'List_price_product',
+    'version': '',
+    'summary': """ List_price_product Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
