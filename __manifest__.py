d7bc24004bb7d4c1b97905a092fbad68b8d0711b
Breithner <Breithnerap@gmail.com>
2025-07-11 13:34:42 -0500

    upload change

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'List_price_product',
-    'version': '',
-    'summary': """ List_price_product Summary """,
-    'author': '',
+    'name': 'Lista de Precios - Multicompañia',
+    'version': '1.0',
+    'summary': """ Modulo que permite actualizar todos los productos de las diferentes compañias con el mismo codigo  """,
+    'author': 'Breithner Aquituari',
     'website': '',
     'category': '',
     'depends': ['base', ],
